5a9413e9514d27b5c7da84c97c293dbf698ac406
Efremov Alexey <lexich121@gmail.com>
2016-12-29 10:42:06 +0300

    Fix: scoping for postcss-css-vars

--- tests/fixtures/postcss-css-vars/helper.ts
+++ tests/fixtures/postcss-css-vars/helper.ts
@@ -3,18 +3,35 @@ var parser = require('postcss-value-parser');
 export function valueProcessor(value: string, vars: any): string {
     if (!vars) { return value; }
     return parser(value).walk((node: any)=> {
-        if (node.type === 'function' && node.value === 'var') {            
-            if (node.nodes.length === 1) {
+        if (node.type === 'function' && node.value === 'var') {
+            const len = node.nodes.length;
+            if (len >= 1) {
                 const wordNode = node.nodes[0];
                 if (wordNode.type !== 'word') { 
                     return;
                 }
                 const revar = vars[wordNode.value];
-                if (!revar) { return; }
-                node.type = 'word';
-                node.value = revar;
-                node.nodes = undefined;
+                if (revar) {
+                    node.value = revar;
+                } else {
+                    if (len >= 3) {
+                        if (node.nodes[1].type !== "div" || node.nodes[1].value !== ",") {
+                            return;
+                        }
+                        const wordNode = node.nodes[2];
+                        if (wordNode.type !== 'word') { 
+                            return;
+                        }
+                        node.value = vars[wordNode.value] || wordNode.value;
+                    } else {
+                        return;   
+                    }
+                }
+            } else {
+                node.value = "";
             }
+            node.type = 'word';
+            node.nodes = undefined;
         }
     }).toString();
 }
